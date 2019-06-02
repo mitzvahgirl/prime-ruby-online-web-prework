c55f260160f029ae0f13ce6cbf7fb87decdf19f5
Amy Fogelman <abfnj1@gmail.com>
2019-06-02 10:50:31 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,7 +2,15 @@
 
 require prime
 
-Prime.each(25) do (prime)
-  p prime
+def is_prime(num)
+  (2..(num - 1)).each do |n|
+    return false if num % n == 0
+  end
+  true
 end
-  
+
+is_prime(7)
+#=> true
+
+is_prime(4)
+#=> false
