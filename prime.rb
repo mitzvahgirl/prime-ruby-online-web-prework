d06ad0b989a369088c3dd0219311ffc35596f3c3
Amy Fogelman <abfnj1@gmail.com>
2019-06-02 10:51:11 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
 # Add  code here!
+
+require prime
+
+def is_prime(num)
+  (2..(num - 1)).each do (n)
+    return false if num % n == 0
+  end
+  true
+end
+
+is_prime(7)
+#=> true
+
+is_prime(4)
+#=> false
